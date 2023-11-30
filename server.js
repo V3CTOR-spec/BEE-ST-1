d114bda1c13567e4d284c56ab14deb9e7bec4acd
VECTOR-spec <princeraj6884@gmail.com>
2023-11-30 14:34:17 +0530

    BEE ST 1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const mongoURI = 'mongodb+srv://rajmahchawal:rajmachawal@cluster0.t1lyuje.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURI);
 mongoose.connection.on('error', (error) => console.error('MongoDB connection error:', error));
 
 app.use(bodyParser.json());
@@ -24,5 +24,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
